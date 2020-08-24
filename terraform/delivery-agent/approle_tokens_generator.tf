03bfb545b805dcff30ff1bac4823fc44168dd93b
binsabbar <ibinsabbar@gmail.com>
2020-08-24 22:18:08 +0300

    add token generator

---
+++ terraform/delivery-agent/approle_tokens_generator.tf
@@ -0,0 +1,23 @@
+variable approles {
+  type = map(object({
+    path      = string
+    role_id   = string
+    secret_id = string
+  }))
+}
+
+variable token {
+  type = string
+}
+
+provider "vault" {
+  address = "http://localhost:8200"
+  token   = var.token
+}
+
+resource "vault_approle_auth_backend_login" "login" {
+  for_each  = var.approles
+  backend   = each.value.path
+  role_id   = each.value.role_id
+  secret_id = each.value.secret_id
+}
