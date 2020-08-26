41dc64110e96e8430671f864a91568c7bce74026
binsabbar <ibinsabbar@gmail.com>
2020-08-26 20:21:42 +0300

    generate ca and nodes certs in Terraform

---
+++ docker/vault/certs/vault_ca.tf
@@ -0,0 +1,64 @@
+resource "tls_private_key" "vault_ca_key" {
+  algorithm = "RSA"
+  rsa_bits = 4048
+}
+
+locals {
+  cert_period = 24*30*12
+}
+resource "tls_self_signed_cert" "vault_ca_cert" {
+  key_algorithm   = "RSA"
+  private_key_pem = tls_private_key.vault_ca_key.private_key_pem
+
+  subject {
+    common_name  = "vault.local"
+    organization = "Mo's Inc"
+    street_address = []
+  }
+
+  validity_period_hours = local.cert_period
+
+  allowed_uses = [
+    "digital_signature",
+    "crl_signing",
+    "cert_signing"
+  ]
+}
+
+resource "tls_locally_signed_cert" "vault_node_cert" {
+  for_each = local.vault_nodes
+
+  cert_request_pem   = tls_cert_request.vault_node_csr[each.key].cert_request_pem
+
+  ca_key_algorithm   = tls_self_signed_cert.vault_ca_cert.key_algorithm
+  ca_private_key_pem = tls_private_key.vault_ca_key.private_key_pem
+  ca_cert_pem        = tls_self_signed_cert.vault_ca_cert.cert_pem
+
+  validity_period_hours = local.cert_period
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+}
+
+resource "local_file" "vault_ca_private_key" {
+    content     = tls_private_key.vault_ca_key.private_key_pem
+    filename = "${path.module}/vault-ca-key.pem"
+    file_permission = "0600"
+}
+
+resource "local_file" "vault_ca_cert" {
+  content     = tls_self_signed_cert.vault_ca_cert.cert_pem 
+  filename = "${path.module}/vault-ca-cert.pem"
+  file_permission = "0600"
+}
+
+resource "local_file" "vault_node_cert" {
+  for_each = local.vault_nodes
+
+  content     = tls_locally_signed_cert.vault_node_cert[each.key].cert_pem 
+  filename = "${path.module}/${each.key}-cert.pem"
+  file_permission = "0600"
+}
---
+++ docker/vault/certs/vault_nodes.tf
@@ -0,0 +1,43 @@
+locals {
+  vault_nodes = {
+    "vault-node-1" = {
+      uris = ["vault-1"]
+      dns_names = ["vault-1.localhost"]
+      common_name  = "vault-1"
+    }
+
+    "vault-node-2" = {
+      uris = ["vault-2"]
+      dns_names = ["vault-2.localhost"]
+      common_name  = "vault-2"
+    }
+  }
+}
+
+resource "tls_private_key" "vault_node_key" {
+  for_each = local.vault_nodes
+
+  algorithm = "RSA"
+  rsa_bits = 4048
+}
+
+resource "tls_cert_request" "vault_node_csr" {
+  for_each = local.vault_nodes
+
+  key_algorithm   = tls_private_key.vault_node_key[each.key].algorithm
+  private_key_pem = tls_private_key.vault_node_key[each.key].private_key_pem
+  uris = each.value.uris
+  dns_names = each.value.dns_names
+  subject {
+    common_name  = each.value.common_name
+    organization = "Mo's Inc"
+  }
+}
+
+resource "local_file" "vault_node_key" {
+  for_each = local.vault_nodes
+
+  content     = tls_private_key.vault_node_key[each.key].private_key_pem
+  filename = "${path.module}/${each.key}-key.pem"
+  file_permission = "0600"
+}
