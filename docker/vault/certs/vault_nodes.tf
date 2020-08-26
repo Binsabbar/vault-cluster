1a3f0e9afe8c99714b645737469e7f77c6262f42
binsabbar <ibinsabbar@gmail.com>
2020-08-26 20:46:44 +0300

    terraform fmt

--- docker/vault/certs/vault_nodes.tf
+++ docker/vault/certs/vault_nodes.tf
@@ -1,15 +1,15 @@
 locals {
   vault_nodes = {
     "vault-node-1" = {
-      uris = ["vault-1"]
-      dns_names = ["vault-1.localhost"]
-      common_name  = "vault-1"
+      uris        = ["vault-1"]
+      dns_names   = ["vault-1.localhost"]
+      common_name = "vault-1"
     }
 
     "vault-node-2" = {
-      uris = ["vault-2"]
-      dns_names = ["vault-2.localhost"]
-      common_name  = "vault-2"
+      uris        = ["vault-2"]
+      dns_names   = ["vault-2.localhost"]
+      common_name = "vault-2"
     }
   }
 }
@@ -18,7 +18,7 @@ resource "tls_private_key" "vault_node_key" {
   for_each = local.vault_nodes
 
   algorithm = "RSA"
-  rsa_bits = 4048
+  rsa_bits  = 4048
 }
 
 resource "tls_cert_request" "vault_node_csr" {
@@ -26,8 +26,8 @@ resource "tls_cert_request" "vault_node_csr" {
 
   key_algorithm   = tls_private_key.vault_node_key[each.key].algorithm
   private_key_pem = tls_private_key.vault_node_key[each.key].private_key_pem
-  uris = each.value.uris
-  dns_names = each.value.dns_names
+  uris            = each.value.uris
+  dns_names       = each.value.dns_names
   subject {
     common_name  = each.value.common_name
     organization = "Mo's Inc"
@@ -37,7 +37,7 @@ resource "tls_cert_request" "vault_node_csr" {
 resource "local_file" "vault_node_key" {
   for_each = local.vault_nodes
 
-  content     = tls_private_key.vault_node_key[each.key].private_key_pem
-  filename = "${path.module}/${each.key}-key.pem"
+  content         = tls_private_key.vault_node_key[each.key].private_key_pem
+  filename        = "${path.module}/${each.key}-key.pem"
   file_permission = "0600"
 }
