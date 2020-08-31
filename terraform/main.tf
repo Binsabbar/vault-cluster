ed269b438be4d4ab718131203abbbeefba3c7e81
Mo <Binsabbar@users.noreply.github.com>
2020-08-31 23:12:46 +0300

    delete terraform for vault

--- terraform/main.tf
+++
@@ -1,5 +0,0 @@
-provider "vault" {
-  address      = var.address
-  token        = var.token
-  ca_cert_file = var.ca_cert_file
-}
--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable token {
-  type = string
-}
-variable address {
-  type = string
-}
-variable ca_cert_file {
-  type = string
-}
