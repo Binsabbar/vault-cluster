f95cae7f05520da78b33d625b8afdbcc52e82498
Mo <Binsabbar@users.noreply.github.com>
2020-08-31 23:42:37 +0300

    add identiy to the app role

--- vault-main/approle/main.tf
+++ vault-main/approle/main.tf
@@ -15,3 +15,13 @@ resource "vault_approle_auth_backend_role" "cicd" {
   secret_id_num_uses = 1 
   # secret_id_ttl = 100 # The time to live for the secret id before it is destoryed
 }
+
+resource "vault_identity_entity" "cicd_entity" {
+  name     = "cicd"
+}
+
+resource "vault_identity_entity_alias" "cicd_entity_alias" {
+  name           = vault_approle_auth_backend_role.cicd.role_name
+  mount_accessor = vault_auth_backend.approle.accessor
+  canonical_id = vault_identity_entity.cicd_entity.id
+}
