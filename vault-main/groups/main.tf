2d300d96b91fec824be7cfcb611cf3c68f811825
Mo <Binsabbar@users.noreply.github.com>
2020-08-31 23:16:05 +0300

    create group and policie module

---
+++ vault-main/groups/main.tf
@@ -0,0 +1,41 @@
+resource "vault_identity_group" "admins" {
+  name              = "admins"
+  type              = "internal"
+  policies          = [vault_policy.admins.name]
+}
+
+resource "vault_identity_group" "default" {
+  name              = "default"
+  type              = "internal"
+  policies          = ["default"]
+}
+
+resource "vault_identity_group" "secret_fetcher" {
+  name              = "secret_fetcher"
+  type              = "internal"
+  policies          = []
+}
+
+resource "vault_identity_group" "approle_token_generator" {
+  name              = "approle_token_generator"
+  type              = "internal"
+  policies          = []
+}
+
+resource "vault_identity_group" "development" {
+  name              = "development"
+  type              = "internal"
+  policies          = []
+}
+
+resource "vault_identity_group" "uat" {
+  name              = "uat"
+  type              = "internal"
+  policies          = []
+}
+
+resource "vault_identity_group" "production" {
+  name              = "prod"
+  type              = "internal"
+  policies          = []
+}
---
+++ vault-main/variables.tf
@@ -0,0 +1,9 @@
+variable token {
+  type = string
+}
+variable address {
+  type = string
+}
+variable ca_cert_file {
+  type = string
+}
