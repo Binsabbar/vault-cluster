3f24d49f8a865df5d52f20092dcb100e59a5631d
Mo <Binsabbar@users.noreply.github.com>
2020-09-02 00:35:27 +0300

    change to wrapped token instead of secret id

--- vault-secret-id-generator/output.tf
+++ vault-secret-id-generator/output.tf
@@ -1,6 +1,6 @@
-output "approles" {
+output "wrapped_token" {
   value = {
     for k, v in vault_approle_auth_backend_role_secret_id.secret_id:
-      "${v.role_name}" => v.secret_id
+      "${v.role_name}" => v.wrapping_token
   }
 }
