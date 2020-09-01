b6a4461e336cca100a51d744e6a8dbbcb370c7e5
Mo <Binsabbar@users.noreply.github.com>
2020-09-02 00:18:25 +0300

    terraform fmt

--- vault-main/groups/outputs.tf
+++ vault-main/groups/outputs.tf
@@ -1,6 +1,6 @@
 output "groups" {
-    value = {
-        admins = vault_identity_group.admins
-        default = vault_identity_group.default
-    }
+  value = {
+    admins  = vault_identity_group.admins
+    default = vault_identity_group.default
+  }
 }
