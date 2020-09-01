150f10e2610d4fbabf314d90fc789b0d2ff671c9
binsabbar <ibinsabbar@gmail.com>
2020-09-02 00:29:45 +0300

    create secret id generator

---
+++ vault-secret-id-generator/variables.tf
@@ -0,0 +1,15 @@
+variable "role_ids" {
+  type = list(string)
+}
+variable "backend_path" {
+  type = string
+}
+variable token {
+  type = string
+}
+variable address {
+  type = string
+}
+variable ca_cert_file {
+  type = string
+}
