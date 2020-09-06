96da8373322ddf09c346d1dfa90b28013f938eb3
binsabbar <ibinsabbar@gmail.com>
2020-09-06 23:01:33 +0300

    create new group + policy

--- vault-main/groups/main.tf
+++ vault-main/groups/main.tf
@@ -2,40 +2,58 @@ resource "vault_identity_group" "admins" {
   name     = "admins"
   type     = "internal"
   policies = [vault_policy.admins.name]
+
+  lifecycle {
+    ignore_changes = ["member_entity_ids"]
+  }
 }
 
 resource "vault_identity_group" "default" {
   name     = "default"
   type     = "internal"
   policies = ["default"]
+
+  lifecycle {
+    ignore_changes = ["member_entity_ids"]
+  }
 }
 
-resource "vault_identity_group" "secret_fetcher" {
-  name     = "secret_fetcher"
+resource "vault_identity_group" "wrapped_secret_id_generator" {
+  name     = "wrapped_secret_id_generator"
   type     = "internal"
-  policies = []
-}
+  policies = [vault_policy.wrapped_secret_id_generator.name]
 
-resource "vault_identity_group" "approle_token_generator" {
-  name     = "approle_token_generator"
-  type     = "internal"
-  policies = []
+  lifecycle {
+    ignore_changes = ["member_entity_ids"]
+  }
 }
 
 resource "vault_identity_group" "development" {
   name     = "development"
   type     = "internal"
   policies = []
+
+  lifecycle {
+    ignore_changes = ["member_entity_ids"]
+  }
 }
 
 resource "vault_identity_group" "uat" {
   name     = "uat"
   type     = "internal"
   policies = []
+
+ lifecycle {
+    ignore_changes = ["member_entity_ids"]
+  }
 }
 
 resource "vault_identity_group" "production" {
   name     = "prod"
   type     = "internal"
   policies = []
+
+ lifecycle {
+    ignore_changes = ["member_entity_ids"]
+  }
 }
--- vault-main/groups/policies.tf
+++ vault-main/groups/policies.tf
@@ -3,3 +3,9 @@ resource "vault_policy" "admins" {
 
   policy = file("${path.module}/policies/admin.hcl")
 }
+
+resource "vault_policy" "wrapped_secret_id_generator" {
+  name = "wrapped-secret-id-generator"
+
+  policy = file("${path.module}/policies/wrapped-secret-generator.hcl")
+}
